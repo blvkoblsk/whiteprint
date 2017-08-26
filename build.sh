ece45cfdf519cd65a86b946c5fbd589c2cada45a
synesenom <enys.mones@gmail.com>
2017-08-26 18:03:28 +0200

    minor fixes

--- build.sh
+++ build.sh
@@ -3,7 +3,5 @@
 SRC_DIR="src"
 
 # Compile LESS code
-lessc ${SRC_DIR}/bundle.less wp.css
-
-# Minify CSS
-uglifycss wp.css --output wp.min.css
+lessc --clean-css \
+    ${SRC_DIR}/bundle.less wp.min.css
